38993395921f71c9ca0b805605b6d077ca23fdec
Gus <yo@soygus.com>
2019-10-28 18:21:13 -0600

    Adds search container UI

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,4 +1,16 @@
-import { Box, Container, createStyles, Grid } from "@material-ui/core";
+import { theme as aragonTheme } from "@aragon/ui";
+import {
+  Box,
+  Container,
+  createStyles,
+  Divider,
+  Grid,
+  IconButton,
+  InputBase,
+  Paper,
+  Typography,
+} from "@material-ui/core";
+import SearchIcon from "@material-ui/icons/Search";
 import { withStyles } from "@material-ui/styles";
 import React from "react";
 import { theme } from "../../theme";
@@ -36,13 +48,35 @@ const styles = createStyles({
       paddingRight: 0,
     },
   },
-  gtqBtcButton: {
-    padding: 0,
-    fontSize: theme.typography.h5.fontSize,
-    color: theme.palette.primary.main,
-    [theme.breakpoints.down("sm")]: {
-      ...theme.typography.h5,
-    },
+  main: {
+    backgroundColor: aragonTheme.secondaryBackground,
+    minHeight: "100vh",
+  },
+  searchContainer: {
+    backgroundColor: aragonTheme.infoBackground,
+    minHeight: "35vh",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+  },
+  root: {
+    padding: "2px 4px",
+    display: "flex",
+    alignItems: "center",
+  },
+  input: {
+    marginLeft: theme.spacing(1),
+    flex: 1,
+  },
+  iconButton: {
+    padding: 10,
+  },
+  divider: {
+    height: 28,
+    margin: 4,
+  },
+  searchInputContainer: {
+    marginTop: theme.spacing(3),
   },
 });
 
@@ -50,9 +84,27 @@ export const Home = withStyles(styles)(({ classes }: { classes: any }) => {
   return (
     <>
       <Header />
-      <Box bgcolor="primary.main">
-        <Container maxWidth="xl">
-          <Grid container></Grid>
+      <Box bgcolor="primary.main" className={classes.main}>
+        <Container maxWidth="xl" className={classes.searchContainer}>
+          <Typography align="center" variant="h3" gutterBottom>
+            Aragon Block Explorer
+          </Typography>
+          <Grid container justify="center" className={classes.searchInputContainer}>
+            <Grid item xs={12} lg={6}>
+              <Paper className={classes.root}>
+                <InputBase
+                  className={classes.input}
+                  placeholder="Block no. or tx id..."
+                  inputProps={{ "aria-label": "txid" }}
+                  autoFocus
+                />
+                <Divider className={classes.divider} orientation="vertical" />
+                <IconButton className={classes.iconButton} aria-label="search">
+                  <SearchIcon />
+                </IconButton>
+              </Paper>
+            </Grid>
+          </Grid>
         </Container>
       </Box>
     </>
